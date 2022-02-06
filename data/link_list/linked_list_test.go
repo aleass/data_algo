869f695be7377160f6a52c5182f99daf237a64ee
Tsuki <g614547908@gmail.com>
2022-02-07 03:46:30 +0800

    singlyLinkedList base opertion

---
+++ data/link_list/linked_list_test.go
@@ -0,0 +1,29 @@
+package link_list
+
+import (
+	"fmt"
+	"testing"
+)
+
+// singly linked list
+func TestSinglyLinkedList(t *testing.T) {
+	var link = NewSingly()
+	link.AddSingly(0, &SinglyLinkedList{
+		Id:   1,
+		Data: "i am singly 1",
+	})
+	link.AddSingly(20, &SinglyLinkedList{
+		Id:   21,
+		Data: "i am singly 50",
+	})
+	link.AddSingly(1, &SinglyLinkedList{
+		Id:   31,
+		Data: "i am singly 31",
+	})
+	res, _ := link.ReadSingly(31)
+	fmt.Println(res)
+	link.DeleteOrChangeSingly(31, &SinglyLinkedList{
+		Id:   99,
+		Data: "i am singly 99",
+	})
+}
---
+++ data/link_list/singly_linked_list.go
@@ -0,0 +1,65 @@
+package link_list
+
+type SinglyLinkedList struct {
+	Id      int
+	NextPor *SinglyLinkedList
+	Data    string
+}
+
+// NewSingly return a nil head of SinglyLinkedList
+func NewSingly() *SinglyLinkedList {
+	return &SinglyLinkedList{}
+}
+
+// ReadSingly return index equal id of value
+func (s *SinglyLinkedList) ReadSingly(id int) (*SinglyLinkedList, error) {
+	if s.NextPor == nil {
+		return nil, nil
+	}
+	var p = s.NextPor
+	for p.NextPor != nil && p.Id != id {
+		p = p.NextPor
+	}
+	if p.Id != id {
+		p = nil
+	}
+	return p, nil
+}
+
+// AddSingly add a SinglyLinkedList from index id of next
+func (s *SinglyLinkedList) AddSingly(id int, val *SinglyLinkedList) error {
+	if s.NextPor == nil {
+		s.NextPor = val
+		return nil
+	}
+	var p = s.NextPor
+	for p.NextPor != nil && p.Id != id {
+		p = p.NextPor
+	}
+	if p.Id == id {
+		val.NextPor = p.NextPor
+	}
+	p.NextPor = val
+	return nil
+}
+
+func (s *SinglyLinkedList) DeleteOrChangeSingly(id int, val *SinglyLinkedList) error {
+	if s.NextPor == nil {
+		return nil
+	}
+	var p = s.NextPor
+	var pre = s
+	for p.NextPor != nil && p.Id != id {
+		pre = p
+		p = p.NextPor
+	}
+	if p.Id == id {
+		if val != nil {
+			val.NextPor = p.NextPor
+			p.NextPor = val
+		}
+		pre.NextPor = p.NextPor
+	}
+
+	return nil
+}
